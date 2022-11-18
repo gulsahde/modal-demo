fff38db84b39729fdb254706e37d916e6b341973
GulsahDgr <34721537+GulsahDgr@users.noreply.github.com>
2022-11-18 10:24:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./components/Todo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Cart</h1>
+      <Todo text="Kitchen Island - $1.500"/>
+      <Todo text="Sideboard - $1.100"/>
+      <Todo text="Chair - $100"/>
     </div>
   );
 }
